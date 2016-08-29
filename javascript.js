d3eb0782a5bef3a461cfce6bcccd810609f60cff
Xalicus Xander <Links.Retreat@Gmail.com>
2016-08-29 13:59:24 -0700

    Uploaded my files I've been working on.
    Initial commit after creating repo and uploading files.

---
+++ javascript.js
@@ -0,0 +1,21 @@
+/*
+CIS166AA Chapter 1 Case Project
+Author: Kevin Ward
+Date: 8/28/2016
+Filename: javascript.js
+*/
+
+//define any variables I will need in my page.
+// Legend of Zelda copyright Nintendo.
+var crest = "images/hyrulecrest.png";
+var rinku = "images/link.jpg";
+var zelda = "images/zelda.jpg";
+var ganon = "images/ganon.jpg";
+// Star Wars characters copyright Disney.
+var luke = "images/luke.jpg";
+var leia = "images/leia.jpg";
+var vader = "images/vader.jpg";
+// Video Game toons copyright their respective owners.
+var xal1 = "images/xal1.png";
+var xal2 = "images/xal2.jpg";
+var xal3 = "images/xal3.jpg";
