004f86c4faa4eb90c27f5b760ab5f0aeff0f39b4
Xalicus Xander <Links.Retreat@Gmail.com>
2016-09-19 21:32:01 -0700

    Monday update
    I can't get my form to work, but I did add exception handling, and the comment summary.

--- javascript.js
+++ javascript.js
@@ -23,14 +23,25 @@ var xal1 = "images/xal1.png";
 var xal2 = "images/xal2.jpg";
 var xal3 = "images/xal3.jpg";
 
-
 //	Variables for my DragonBall Z Power Level function.
 var totalPwrLvl = 0;
 var pwrLvlCost = 0;
-var plage = 18;
-var plfriends = 1;
+var age = document.getElementById("age");
 var over9k = false;
 var alien = false;
+
+//	Global vars tracking status of each form sections.
+var ageComp = true;
+var over9kComp = true;
+var alienComp = true;
+//	Global var referencing sidebar p element.
+var pwrlvlSide = document.getElementById("powerlvl");
+//	Global var referencing fieldset elements.
+var ageFieldset = document.getElementsByTagName("fieldset")[0];
+var over9kFieldset = document.getElementsByTagName("fieldset")[1];
+var alienFieldset = document.getElementsByTagName("fieldset")[2];
+//	Global vars referencing text input elements.
+var ageBox = document.getElementById("age");
 
 
 //	Arrays for the if/else/switch/while statement.
@@ -38,61 +49,64 @@ var whoType = ["Classic Doctor Who", "-", "Rebooted Doctor Who"];
 var docWho = ["William Hartnell", "Patrick Troughton", "Jon Pertwee", "Tom Baker", 
 				"Peter Davison", "Colin Baker", "Sylvester McCoy", "Paul McGann", 
 				"John Hurt", "Christopher Eccleston", "David Tennant", "Matt Smith", "Peter Capaldi", 
-				"Rupert Grint", "Doctor 14", "Doctor 15"];
+				"Doctor 13", "Doctor 14", "Doctor 15"];
 var whoYrs = ["1963-66", "1966-69", "1970-74", "1974-81", "1982-84", "1984-86", 
 				"1987-89", "1996", "2013", "2005", "2005-10", "2010-13", "2014-present", 
-				"Possible Future", "Timey Whimey", "Wibbly Wobbly"];
+				"Future Time", "Timey Whimey", "Wibbly Wobbly"];
 var docType = ["classic", "classic", "classic", "classic", "classic", "classic", 
 				"classic", "classic", "reboot", "reboot", "reboot", "reboot", "reboot",
 				"future", "future", "future"];
+var thHead = document.getElementsByTagName("th");
 
 
 //	Calculates your DragonBall Z Power Level.
 function calcPwrLvl() {
-	var age = document.getElementById("plage");
-console.log("After age is declared: " + age.value);
-	var friends = document.getElementById("plfriends");
-console.log("After friends are declared: " + friends.value);
+	var plage = ageBox;			//document.getElementById("age");
+//window.alert(plage.value + ageBox.value + ageFieldset.value + age.value);
 	totalPwrLvl -= pwrLvlCost;
-console.log("Before pwrLvlCost: " + totalPwrLvl.value);
-	pwrLvlCost = age.value * 20 * friends.value;
+	pwrLvlCost = plage * 20; //	For some reason this line is causing a NaN error or just doesn't change the pwrLvlCost.
 	totalPwrLvl += pwrLvlCost;
-console.log("After pwrLvlCost: " + totalPwrLvl.value);
-	document.getElementById("powerlvl").innerHTML = "#" + totalPwrLvl;
+	pwrlvlSide.innerHTML = "#" + totalPwrLvl;
 }
 
 
 //	Adds/subtracts over 9000 Power Levels.
 function toggle9K() {
 	(document.getElementById("over9k").checked === false) ? totalPwrLvl -= 9000 : totalPwrLvl += 9000;
-	document.getElementById("powerlvl").innerHTML = "#" + totalPwrLvl;
-console.log("After toggle9K: " + totalPwrLvl.value);
+	pwrlvlSide.innerHTML = "#" + totalPwrLvl;
 }
 
 
 //	Adds/subtracts if they are an alien or not.
 function toggleAlien() {
 	(document.getElementById("alien").checked === false) ? totalPwrLvl -= 250 : totalPwrLvl += 250;
-	document.getElementById("powerlvl").innerHTML = "#" + totalPwrLvl;
-console.log("After toggleAlien: " + totalPwrLvl.value);
+	pwrlvlSide.innerHTML = "#" + totalPwrLvl;
 }
 
 
 //	Creates event listeners for my Power Level form.
 function createEventListeners() {
-	document.getElementById("plage").addEventListener("change", calcPwrLvl, false);
-	document.getElementById("plfriends").addEventListener("change", calcPwrLvl, false);
-	document.getElementById("over9k").addEventListener("change", toggle9K, false);
-	document.getElementById("alien").addEventListener("change", toggleAlien, false);
+	if (document.addEventListener) {
+		ageFieldset.addEventListener("change", calcPwrLvl, false);
+		over9kFieldset.addEventListener("change", toggle9K, false);
+		alienFieldset.addEventListener("change", toggleAlien, false);
+	} else if (document.attachEvent) {
+		ageFieldset.attachEvent("onchange", calcPwrLvl, false);
+		over9kFieldset.attachEvent("onchange", toggle9K, false);
+		alienFieldset.attachEvent("onchange", toggleAlien, false);
+	}
 }
 
 
 //	This will set all the Power Level form field values to defaults.
 function resetLvl() {
-	document.getElementById("plage").value = 18;
-	document.getElementById("plfriends").value = 1;
-	document.getElementById("over9k").checked = over9k;
-	document.getElementById("alien").checked = alien;
+//	document.getElementById("age").value = 18;
+//	document.getElementById("friends").value = 1;
+//	document.getElementById("over9k").checked = over9k;
+//	document.getElementById("alien").checked = alien;
+	ageFieldset.value = age;
+	over9kFieldset.checked = over9k;
+	alienFieldset.checked = alien;
 	calcPwrLvl();
 	createEventListeners();
 }
@@ -102,16 +116,16 @@ function resetLvl() {
 function addWhoTitle() {
 	var i = 0;
 	while (i < whoType.length) {
-		document.getElementsByTagName("th")[i].innerHTML = whoType[i];
+		thHead[i].innerHTML = whoType[i];
 		i++;
 	}
 }
 
 
 //	This switch statement should add the Doctor Who names and dates.
-function addYrs() {
+function addDocYrs() {
 	var	docYrs = "";
-	for (var i = 0; i < 13; i++) {
+	for (var i = 0; i < docWho.length; i++) {
 		var tableCell = document.getElementsByTagName("td");
 		switch (docType[i]) {
 			case "classic":
@@ -139,11 +153,62 @@ function addYrs() {
 } // end function
 
 
-//	This function will populate the Doctor Who page and reset the Power Level form.
+//	The function should validate the age in the Power Level form from getting text in the number spots.
+function valAgeForm() {
+	var valAge = age;
+	var validity = true;
+	var messageText = "";
+	
+	try {
+		if (!(valAge.value <= 18)) {
+			throw "Please enter a valid age greater than 18.";
+		}//	end if
+	}// end try
+	
+	catch(ageError) {
+		window.alert(ageError);
+		return false;
+	}// end catch
+	
+	finally {
+		ageValid = true;
+	}// end finally
+	
+	return true;
+}// end function
 
+/*
+function valFrdForm() {
+	var valFrd = friendBox;
+	var validity = true;
+	var messageText = "";
+	
+	try {
+		if (!(valFrd.value >= 1)) {
+			throw "Please get more friends.";
+		}// end if
+	}// end try
+	
+	catch(frdError) {
+		window.alert(frdError);
+		return false;
+	}// end catch
+	
+	finally {
+		frdValid = true;
+	}// end finally
+	
+	return true;
+}// end function
+*/
+
+//	This function will populate the Doctor Who page and reset the Power Level form.
 function setUpPage() {
 	addWhoTitle();
-	addYrs();
+	addDocYrs();
+	createEventListeners();
+	valAgeForm();
+//	valFrdForm();
 	resetLvl();
 }
 
